de58ca795fea46ecdac972b77e1db6fbc5232594
Ricardo Malafaia <rbm4@cin.ufpe.br>
2018-04-22 21:59:36 +0000

    migrations fix

--- db/migrate/20180422211321_change_premiados.rb
+++
@@ -1,6 +0,0 @@
-class ChangePremiados < ActiveRecord::Migration[5.2]
-  def change
-      add_column :premiados, :premio_entregue, :boolean
-      add_column :tickets, :position, :string
-  end
-end
--- db/migrate/20180422213256_change_hotfix.rb
+++
@@ -1,6 +0,0 @@
-class ChangeHotfix < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :tickets, :position
-    add_column :premiados, :position, :string
-  end
-end
