48619efaa9dcc5979d48dcf7b2d179d91c3e0b69
Ricardo Malafaia <rbm4@cin.ufpe.br>
2018-04-24 14:20:54 +0000

    ajsute de calculo de premios

--- config/initializers/bit8.rb
+++ config/initializers/bit8.rb
@@ -1,3 +1,3 @@
-PROPORTIONS = [40,20,10,8,6,5,4,3,2,1]
+PROPORTIONS = [55,25,10,5,4]
 CURRENCIES = ["BTC","DOGE","LTC","ETH","DASH","BCH","DGB", "GRC"]
-COST = {BTC: BigDecimal("0.001"), DOGE: BigDecimal("1"), LTC: BigDecimal("0.004"), BCH: BigDecimal("0.0004"), DASH: BigDecimal("0.002"), ETH: BigDecimal("0.0024"), DGB: BigDecimal("0.002"),  GRC: BigDecimal("0.0002")}
+COST = {BTC: BigDecimal("0.0005"), DOGE: BigDecimal("1"), LTC: BigDecimal("0.004"), BCH: BigDecimal("0.0004"), DASH: BigDecimal("0.002"), ETH: BigDecimal("0.0024"), DGB: BigDecimal("0.002"),  GRC: BigDecimal("0.0002")}
